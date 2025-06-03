6644398bcc2921bf7d634e5a55a5bf0e1bd4677f
Halfgork <gorkemsakalak@gmail.com>
2025-06-04 02:00:33 +0300

    Update configuration files and dependencies for Soroban token contract integration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    // appDir is now the default in Next.js 14, no need to specify
   },
   webpack: (config) => {
     config.experiments = {
